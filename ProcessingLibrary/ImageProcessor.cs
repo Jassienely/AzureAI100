f309823e9fff2f00c6f218e505cf03778216dbf5
jassienely <jasmine.nely@gmail.com>
2021-01-15 15:08:14 +0800

    first commit

---
+++ ProcessingLibrary/ImageProcessor.cs
@@ -0,0 +1,52 @@
+﻿// Below each "Step" there is code that should be entered
+// Corresponds to the Steps in the instructions for 2_ImageProcessor.md
+// Step 1: Add the using directives below:
+
+// Specify the namespace
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using ServiceHelpers;
+
+namespace ProcessingLibrary
+{
+    // Specify the class
+    public class ImageProcessor
+    {
+        // Step 2: Create the ProcessImageAsync method:
+
+        public static async Task<ImageInsights> ProcessImageAsync(string imgPath, string imageId)
+        {
+            // Set up an array that we'll fill in over the course of the processor:
+            VisualFeatureTypes?[] DefaultVisualFeaturesList = new VisualFeatureTypes?[] { VisualFeatureTypes.Tags, VisualFeatureTypes.Description };
+
+            // Call the Computer Vision service and store the results in imageAnalysisResult:
+            var imageAnalysisResult = await VisionServiceHelper.AnalyzeImageAsync(imgPath, DefaultVisualFeaturesList);
+
+            // Create an entry in ImageInsights:
+            ImageInsights result = new ImageInsights
+            {
+                ImageId = imageId,
+                Caption = imageAnalysisResult.Description.Captions[0].Text,
+                Tags = imageAnalysisResult.Tags.Select(t => t.Name).ToArray()
+            };
+
+            // Return results:
+            return result;
+        }
+
+
+        // Step 3: Set up an array that we'll fill in over the course of the processor:
+
+        // Step 4: Call the Computer Vision service and store the results in imageAnalysisResult:
+
+        // Step 5: Create an entry in ImageInsights:
+
+        // Step 6: Return results:
+
+    }
+
+}
---
+++ ProcessingLibrary/ServiceHelpers/VisionServiceHelper.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ServiceHelpers
+{
+    public static class VisionServiceHelper
+    {
+        public static int RetryCountOnQuotaLimitError = 10;
+        public static int RetryDelayOnQuotaLimitError = 500;
+
+        private static ComputerVisionClient visionClient { get; set; }
+
+        static VisionServiceHelper()
+        {
+            InitializeVisionService();
+        }
+
+        public static Action Throttled;
+
+        private static string apiKey;
+        public static string ApiKey
+        {
+            get
+            {
+                return apiKey;
+            }
+
+            set
+            {
+                var changed = apiKey != value;
+                apiKey = value;
+                if (changed)
+                {
+                    InitializeVisionService();
+                }
+            }
+        }
+
+        private static string url;
+        public static string Url
+        {
+            get
+            {
+                return url;
+            }
+
+            set
+            {
+                var changed = url != value;
+                url = value;
+                if (changed)
+                {
+                    InitializeVisionService();
+                }
+            }
+        }
+
+        private static void InitializeVisionService()
+        {
+            visionClient = new ComputerVisionClient(new ApiKeyServiceClientCredentials(apiKey)) {
+                Endpoint = url
+            };
+        }
+
+        // handle throttling issues
+        private static async Task<TResponse> RunTaskWithAutoRetryOnQuotaLimitExceededError<TResponse>(Func<Task<TResponse>> action)
+        {
+            int retriesLeft = VisionServiceHelper.RetryCountOnQuotaLimitError;
+            int delay = VisionServiceHelper.RetryDelayOnQuotaLimitError;
+
+            TResponse response = default(TResponse);
+
+            while (true)
+            {
+                try
+                {
+                    response = await action();
+                    break;
+                }
+                catch (Exception exception) when (retriesLeft > 0)
+                {
+                    ErrorTrackingHelper.TrackException(exception, "Vision API throttling error");
+                    if (retriesLeft == 1 && Throttled != null)
+                    {
+                        Throttled();
+                    }
+
+                    await Task.Delay(delay);
+                    retriesLeft--;
+                    delay *= 2;
+                    continue;
+                }
+            }
+
+            return response;
+        }
+
+        // Pull in the methods to call
+        private static async Task RunTaskWithAutoRetryOnQuotaLimitExceededError(Func<Task> action)
+        {
+            await RunTaskWithAutoRetryOnQuotaLimitExceededError<object>(async () => { await action(); return null; });
+        }
+
+        public static async Task<ImageAnalysis> AnalyzeImageAsync(string imageUrl, IList<VisualFeatureTypes?> visualFeatures = null, IList<Details?> details = null)
+        {
+            return await visionClient.AnalyzeImageAsync(imageUrl, visualFeatures, details);
+            // return await RunTaskWithAutoRetryOnQuotaLimitExceededError<ImageAnalysis>(() => visionClient.AnalyzeImageAsync(imageUrl, visualFeatures, details));
+        }
+
+
+    }
+}
